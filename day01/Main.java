4a79a0160fe298382b76d809deb441d5e7871113
Sayonee Das <sayoneesneha@gmail.com>
2025-12-12 17:33:25 +0530

    day01: introduction

---
+++ day01/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args)
+{
+		System.out.println("Sayonee Das");
+		System.out.println("I like drawing.");
+		System.out.println("I want to learn Java because I want to understand how real applications work");
+	}
+}
